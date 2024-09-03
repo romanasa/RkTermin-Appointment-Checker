0e55c772e64e9ada929a312a9ede2f4c8c23b03e
romanasa <>
2024-09-03 10:53:45 +0545

    Gpt fix

--- src/index.ts
+++ src/index.ts
@@ -7,10 +7,12 @@ import { Browser } from "puppeteer";
 import { botToken, delay } from "./utils";
 import { Bot, InputFile } from "grammy";
 import cron from "node-cron";
+
 const bot = new Bot(botToken);
 
 puppeteer.use(StealthPlugin());
 let browser: Browser | null = null;
+
 async function runPuppeteer() {
   try {
     console.log("Started task");
@@ -18,32 +20,28 @@ async function runPuppeteer() {
       headless: true,
       args: ["--no-sandbox", "--disable-setuid-sandbox"],
     });
+
     const page = await browser.newPage();
     await page.setUserAgent(
       "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3419.0 Safari/537.36"
     );
     await page.setDefaultTimeout(2000);
     await page.setDefaultNavigationTimeout(0);
+
     console.log("Going to the page");
-    await Promise.all([
-      page.waitForNavigation({
-        waitUntil: "networkidle2",
-      }),
-      page.goto(
-        "https://service2.diplo.de/rktermin/extern/appointment_showMonth.do?locationCode=kath&realmId=321&categoryId=3142&dateStr=27.09.2024"
-      ),
-    ]);
+    await page.goto(
+      "https://service2.diplo.de/rktermin/extern/appointment_showMonth.do?locationCode=kath&realmId=321&categoryId=3142&dateStr=27.09.2024",
+      { waitUntil: "networkidle2" }
+    );
 
     console.log("Waiting for captcha");
-    // await delay(500);
-    // await page.waitForSelector("captcha");
     const base64Captcha = await page.$eval(
       "captcha>div",
       (div) => div.style.background
     );
     saveBase64ToFile(base64Captcha);
+
     const extractedText = await extractValue();
-    //if not text extracted reload page and check if length of text is less than 5
     if (
       !extractedText ||
       extractedText.length <= 5 ||
@@ -52,31 +50,46 @@ async function runPuppeteer() {
       console.log(extractedText);
       throw new Error("Extraction error");
     }
+
     console.log("Typing captcha");
     await page.focus("#appointment_captcha_month_captchaText");
     await page.keyboard.type(extractedText);
     await delay(4000);
+
     console.log("Clicking submit");
-    await Promise.all([
-      page.waitForNavigation({ waitUntil: "load" }),
-      page.click("#appointment_captcha_month_appointment_showMonth"),
-    ]);
-    console.log("Checking for captcha error");
-    await page.evaluate(() => {
-      console.log("evaluating");
-      const errorContainer = document.querySelector(".global-error");
-      console.log(errorContainer);
-      if (errorContainer) {
-        throw new Error("Captcha error");
+    try {
+      await Promise.all([
+        page.waitForNavigation({ waitUntil: "load" }),
+        page.click("#appointment_captcha_month_appointment_showMonth"),
+      ]);
+    } catch (error) {
+      console.error("Error during navigation:", error.message);
+      if (error.message.includes("detached")) {
+        console.log("Frame was detached, retrying...");
+        await page.reload();
+        return;
+      } else {
+        throw error;
       }
-      return null;
+    }
+
+    console.log("Checking for captcha error");
+    const captchaError = await page.evaluate(() => {
+      const errorContainer = document.querySelector(".global-error");
+      return errorContainer ? true : false;
     });
+
+    if (captchaError) {
+      throw new Error("Captcha error");
+    }
+
     console.log("Checking for availability");
-    const found = await page.evaluate(() =>
-      window.find(
-        "Unfortunately, there are no appointments available at this time. New appointments will be made available for booking at regular intervals."
-      )
-    );
+    const found = await page.evaluate(() => {
+      return document.body.textContent.includes(
+        "Unfortunately, there are no appointments available at this time."
+      );
+    });
+
     if (found) {
       console.log("Not available");
       await bot.api.sendPhoto(
@@ -88,57 +101,54 @@ async function runPuppeteer() {
         `No appointment at this time. \nCaptcha was <b>${extractedText}</b>`,
         { parse_mode: "HTML" }
       );
-      await browser.close();
-      process.exit(1);
     } else {
       console.log("Appointment available");
-      await bot.api.sendMessage(
-        "-1002195384584",
-        "<b>Appointment available be quickkkkkkkkkkkkkkkkkkkkkkkkkkkkkk</b>",
-        { parse_mode: "HTML" }
-      );
-      (async () => {
-        for (let index = 0; index < 25; index++) {
-          await bot.api.sendMessage(
-            "-1002195384584",
-            "<b>Appointment available be quickkkkkkkkkkkkkkkkkkkkkkkkkkkkkk</b>",
-            { parse_mode: "HTML" }
-          );
-        }
-      })();
+      for (let index = 0; index < 25; index++) {
+        await bot.api.sendMessage(
+          "-1002195384584",
+          "<b>Appointment available be quickkkkkkkkkkkkkkkkkkkkkkkkkkkkkk</b>",
+          { parse_mode: "HTML" }
+        );
+      }
       await bot.api.sendMessage(
         "-1002242509001",
         "<b>Appointment available be quickkkkkkkkkkkkkkkkkkkkkkkkkkkkkk</b>",
         { parse_mode: "HTML" }
       );
-      await browser.close();
-      process.exit(1);
     }
+
+    await browser.close();
+    process.exit(1);
+
   } catch (error: any) {
     if (browser) {
-      browser.close();
+      await browser.close();
       browser = null;
     }
+
     if (
       error.message !== "Extraction error" &&
-      error.message !== "Captcha error"
+      error.message !== "Captcha error" &&
+      error.message !== "detached"
     ) {
-      await bot.api.sendMessage("-1002242509001", error.message);
+      await bot.api.sendMessage("-1002242509001", `Unexpected error: ${error.message}`);
+    } else {
+      console.log("Known error occurred, retrying...");
+      setTimeout(runPuppeteer, 5000);
     }
-    console.log(error.message, "err");
-    runPuppeteer();
+
+    console.error("Error occurred:", error.message);
   }
 }
+
 function saveBase64ToFile(base64String: string) {
-  // Split the base64 string to get the actual data
   const base64Data = extractBase64FromBackground(base64String);
   if (!base64Data) {
     return;
   }
-  // Decode the base64 string
+
   const buffer = Buffer.from(base64Data, "base64");
 
-  // Write the buffer to a file
   fs.writeFile(path.join(__dirname, "image.jpg"), buffer, (err) => {
     if (err) {
       console.error("Error writing file:", err);
@@ -149,15 +159,14 @@ function saveBase64ToFile(base64String: string) {
 }
 
 function extractBase64FromBackground(style: string) {
-  // Regular expression to match data URL in the background property
   const regex = /url\(['"]data:image\/[a-zA-Z]+;base64,([a-zA-Z0-9+/=]+)['"]\)/;
   const match = style.match(regex);
 
   if (match && match[1]) {
-    return match[1]; // Return the base64 string
+    return match[1];
   }
 
-  return null; // Return null if no match is found
+  return null;
 }
 
 async function runWithTimeout(timeout: number) {
@@ -172,11 +181,10 @@ async function runWithTimeout(timeout: number) {
 }
 
 console.log("Started app");
-// Schedule to run every 5 minutes at 0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55 minutes
 cron.schedule("*/5 * * * *", async () => {
   try {
     console.log("Started job");
-    await runWithTimeout(290000); // Set timeout to 4.83 minutes (300000 ms)
+    await runWithTimeout(290000);
   } catch (error: any) {
     console.error(error.message);
     console.log("End of 5 minutes timer");
@@ -184,7 +192,6 @@ cron.schedule("*/5 * * * *", async () => {
       console.log("Browser closed");
       await browser.close();
     }
-    process.exit(1); // Exit the process if timeout occurs
+    process.exit(1);
   }
 });
-// runPuppeteer();
