a2c42e7d583b7ca285cbec6f60da945352c55ddf
romanasa <>
2024-11-06 12:43:06 +0100

    Update utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -3,4 +3,5 @@ export async function delay(time: number) {
     setTimeout(resolve, time);
   });
 }
-export const botToken = "7286773036:AAEzmmov-9PTN8HsaY2XPDQHLI6Vbifs-ao";
+//Put your botToken here.
+export const botToken = "";
